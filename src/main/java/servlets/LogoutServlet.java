094fdfeda04032e5df3bccfdfa84f8028fcf9bb9
Alex <alex.cambrini@studio.unibo.it>
2025-07-22 11:58:42 +0200

    refactor logout servlet to handle getWriter exception and improve code safety: - move getWriter inside try block to handle potential IOException - declare authService as static final - preserve existing logic and response flow

--- src/main/java/servlets/LogoutServlet.java
+++ src/main/java/servlets/LogoutServlet.java
@@ -15,25 +15,24 @@ import com.bittercode.service.impl.UserServiceImpl;
 
 public class LogoutServlet extends HttpServlet {
 
-    UserService authService = new UserServiceImpl();
+    private static final UserService authService = new UserServiceImpl();
 
+    @Override
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-        PrintWriter pw = res.getWriter();
         res.setContentType(BookStoreConstants.CONTENT_TYPE_TEXT_HTML);
-        try {
 
+        try {
+            PrintWriter pw = res.getWriter();
             boolean logout = authService.logout(req.getSession());
 
             RequestDispatcher rd = req.getRequestDispatcher("CustomerLogin.html");
             rd.include(req, res);
-//            StoreUtil.setActiveTab(pw, "logout");
+
             if (logout) {
                 pw.println("<table class=\"tab\"><tr><td>Successfully logged out!</td></tr></table>");
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
 }
