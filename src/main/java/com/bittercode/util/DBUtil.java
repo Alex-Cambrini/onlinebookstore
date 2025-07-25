642bd7957628409995a7b3ee59256fc12523e34b
Alex <alex.cambrini@studio.unibo.it>
2025-07-25 14:28:33 +0200

    refactor DBUtil to support testing: - remove static connection to avoid stale connections during tests - create new connection on each call for better test isolation - add private constructor to prevent instantiation - improve exception handling in static block and getConnection method

--- src/main/java/com/bittercode/util/DBUtil.java
+++ src/main/java/com/bittercode/util/DBUtil.java
@@ -8,32 +8,27 @@ import com.bittercode.constant.ResponseCode;
 import com.bittercode.model.StoreException;
 
 public class DBUtil {
-
-    private static Connection connection;
+    private DBUtil() {
+    throw new UnsupportedOperationException("Utility class");
+}
 
     static {
-
         try {
-
             Class.forName(DatabaseConfig.DRIVER_NAME);
-            
-            connection = DriverManager.getConnection(DatabaseConfig.CONNECTION_STRING, DatabaseConfig.DB_USER_NAME,
-                    DatabaseConfig.DB_PASSWORD);
-        } catch (SQLException | ClassNotFoundException e) {
-
-            e.printStackTrace();
-
+        } catch (ClassNotFoundException e) {
+            throw new ExceptionInInitializerError(e);
         }
-
-    }// End of static block
-
-    public static Connection getConnection() throws StoreException {
-
-        if (connection == null) {
-            throw new StoreException(ResponseCode.DATABASE_CONNECTION_FAILURE);
-        }
-
-        return connection;
     }
 
+    public static Connection getConnection() throws StoreException {
+        try {
+            return DriverManager.getConnection(
+                DatabaseConfig.CONNECTION_STRING,
+                DatabaseConfig.DB_USER_NAME,
+                DatabaseConfig.DB_PASSWORD
+            );
+        } catch (SQLException e) {
+            throw new StoreException(ResponseCode.DATABASE_CONNECTION_FAILURE);
+        }
+    }
 }
