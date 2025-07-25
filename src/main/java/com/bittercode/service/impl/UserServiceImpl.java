10273e04c3c4b4528d185ade7e7bdc14af47df6d
Alex <alex.cambrini@studio.unibo.it>
2025-07-25 15:25:32 +0200

    refactor: close PreparedStatement and ResultSet with try-with-resources - fix resource leaks in login() method by closing PreparedStatement and ResultSet properly - apply try-with-resources in register() method for PreparedStatement - rename query constants to uppercase and update SQL insert syntax - correct setString for phone in register() method

--- src/main/java/com/bittercode/service/impl/UserServiceImpl.java
+++ src/main/java/com/bittercode/service/impl/UserServiceImpl.java
@@ -17,33 +17,32 @@ import com.bittercode.util.DBUtil;
 
 public class UserServiceImpl implements UserService {
 
-    private static final String registerUserQuery = "INSERT INTO " + UsersDBConstants.TABLE_USERS
-            + "  VALUES(?,?,?,?,?,?,?,?)";
+    private static final String REGISTER_USER_QUERY = "INSERT INTO " + UsersDBConstants.TABLE_USERS
+            + " (username, password, firstname, lastname, address, phone, mailid, usertype) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
-    private static final String loginUserQuery = "SELECT * FROM " + UsersDBConstants.TABLE_USERS + " WHERE "
+    private static final String LOGIN_USER_QUERY = "SELECT * FROM " + UsersDBConstants.TABLE_USERS + " WHERE "
             + UsersDBConstants.COLUMN_USERNAME + "=? AND " + UsersDBConstants.COLUMN_PASSWORD + "=? AND "
             + UsersDBConstants.COLUMN_USERTYPE + "=?";
 
     @Override
     public User login(UserRole role, String email, String password, HttpSession session) throws StoreException {
         Connection con = DBUtil.getConnection();
-        PreparedStatement ps;
         User user = null;
-        try {
-            String userType = UserRole.SELLER.equals(role) ? "1" : "2";
-            ps = con.prepareStatement(loginUserQuery);
+        String userType = UserRole.SELLER.equals(role) ? "1" : "2";
+        try (PreparedStatement ps = con.prepareStatement(LOGIN_USER_QUERY)) {
             ps.setString(1, email);
             ps.setString(2, password);
             ps.setString(3, userType);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                user = new User();
-                user.setFirstName(rs.getString("firstName"));
-                user.setLastName(rs.getString("lastName"));
-                user.setPhone(rs.getLong("phone"));
-                user.setEmailId(email);
-                user.setPassword(password);
-                session.setAttribute(role.toString(), user.getEmailId());
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    user = new User();
+                    user.setFirstName(rs.getString("firstname"));
+                    user.setLastName(rs.getString("lastname"));
+                    user.setPhone(Long.parseLong(rs.getString("phone")));
+                    user.setEmailId(email);
+                    user.setPassword(password);
+                    session.setAttribute(role.toString(), user.getEmailId());
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -70,14 +69,13 @@ public class UserServiceImpl implements UserService {
     public String register(UserRole role, User user) throws StoreException {
         String responseMessage = ResponseCode.FAILURE.name();
         Connection con = DBUtil.getConnection();
-        try {
-            PreparedStatement ps = con.prepareStatement(registerUserQuery);
+        try (PreparedStatement ps = con.prepareStatement(REGISTER_USER_QUERY)) {
             ps.setString(1, user.getEmailId());
             ps.setString(2, user.getPassword());
             ps.setString(3, user.getFirstName());
             ps.setString(4, user.getLastName());
             ps.setString(5, user.getAddress());
-            ps.setLong(6, user.getPhone());
+            ps.setString(6, String.valueOf(user.getPhone()));
             ps.setString(7, user.getEmailId());
             int userType = UserRole.SELLER.equals(role) ? 1 : 2;
             ps.setInt(8, userType);
